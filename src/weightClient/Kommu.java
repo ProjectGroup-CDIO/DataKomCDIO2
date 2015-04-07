168cc1a9b56438ec960f8f42a9bbf8992df9e4b4
THOMAS <thomas@liljegren.dk>
2015-04-07 16:38:33 +0200

    tredje commit

--- src/weightClient/Kommu.java
+++ src/weightClient/Kommu.java
@@ -1,86 +1,62 @@
 package weightClient;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Reader;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-import javax.swing.*;
 
 public class Kommu {
 
 //	@SuppressWarnings("deprecation")
 	public void kommu() {
 		
-		//Data input
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Input the server IP address: ");
-		String IPinput = (String) scanner.nextLine();
-		System.out.println("Input the server application port number: ");
-		String PortAddress = (String) scanner.nextLine();
-		
-//		JFrame frame = new JFrame("asd");
-		
-//		String IPinput = (String) JOptionPane.showInputDialog(frame, "input IP address");
-//		String PortAdrres = (String) JOptionPane.showInputDialog(frame, "input port address");
-
-		Socket s = null;  
 		DataOutputStream outputStream = null;
 		InputStreamReader inputstream = null;
+		Socket socket = null;
+		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("IP Adress: " +IPinput);
-		System.out.println("Port: " + PortAddress);
+		//Data input
+		System.out.println("Input the server IP address: ");
+		String ip = (String) scanner.nextLine();
+		System.out.println("Input the server application port number: ");
+		String portDest = (String) scanner.nextLine();
+		
+		System.out.println("IP Address: " +ip);
+		System.out.println("Port#: " + portDest);
 
 		try {
-			s = new Socket(IPinput,Integer.parseInt(PortAddress));
-			
-			outputStream = new DataOutputStream(s.getOutputStream());
-			
-			inputstream = new InputStreamReader(s.getInputStream());
-			
+			socket = new Socket(ip,Integer.parseInt(portDest));
+			outputStream = new DataOutputStream(socket.getOutputStream());		
+			inputstream = new InputStreamReader(socket.getInputStream());		
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host: hostname");
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
 		
-		BufferedReader d = new BufferedReader(inputstream);
+		BufferedReader read = new BufferedReader(inputstream);
+		PrintWriter write = new PrintWriter(outputStream,true);
 		
 		//Check for om ports og sockets ikke er null, hvis de ikke er 
-		if(s != null && outputStream != null && inputstream != null){
+		if(socket != null && outputStream != null && inputstream != null){
 			System.out.println("Connection Established");
 			printMenu();
 			//scanner tjekker for om der er en string
-			while(scanner.hasNext()){
-				
-				PrintWriter w = new PrintWriter(outputStream,true);
-				
-				String dataInput = scanner.nextLine();
-//				System.out.println(dataInput);
-				
-				try{
-					
-					w.print(dataInput + "\r\n");
-					w.flush();
-					
-					
-					//this get the reply from the server if any
-					String ReplyMess = d.readLine();
-					if(ReplyMess != null){
-					System.out.println(ReplyMess);
-					}
-					
+			while(scanner.hasNext()){				
+				String dataInput = scanner.nextLine();				
+				try{				
+					write.print(dataInput + "\r\n");
+					write.flush();				
+					//this gets the reply from the server if any
+					String replyMess = read.readLine();
+					if(replyMess != null){
+					System.out.println(replyMess);
+					}				
 				} catch (UnknownHostException e) {
-					System.err.println("Trying to connect to unknown host: " + e);
+					System.err.println("Error001: "+e.getMessage());
 				} catch (IOException e) {
-					System.err.println("IOException:  " + e);
+					System.err.println("Error002: "+e.getMessage());
 				}				
 			}
 			scanner.close();
@@ -92,12 +68,5 @@ public class Kommu {
 		System.out.println("Press Z and ENTER to zero scale");
 		System.out.println("Write D \"<message>\" and ENTER to display message on weight display");
 		System.out.println("Write DW and ENTER to display weight on scale"); //doesnt work?
-	}
-	
-//	public void selectInput(input) {
-//		switch (input) {
-//		case S: 
-//		}
-//	}
-	
+	}	
 }
