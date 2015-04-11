4152d95d1eebd3ecfd450dfea2e7b20acd4db8af
Martin Vieth <martin_123_vieth@hotmail.com>
2015-04-11 10:36:55 +0200

    Ændret weightSim tilbage til det den var oprindeligt. Indsat koden fra clientInput i FTPclient

--- src/ftpClient/FTPClient.java
+++ src/ftpClient/FTPClient.java
@@ -5,6 +5,7 @@ package ftpClient;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -23,6 +24,7 @@ public class FTPClient {
 	BufferedReader in; 
 	DataOutputStream out;
 	Scanner keyb = new Scanner(System.in);
+	private BufferedReader storeText;
 	String request;
 	String response;
 	
@@ -60,6 +62,73 @@ public class FTPClient {
 		System.out.println("Tast 2 for at sende en kommando til ZYBO board");
 		
 	}
+	
+	
+//	private int productVerification(String inline,int nrOfProducts) {
+//		try {
+//			storeText = new BufferedReader(new FileReader("Store.txt"));
+//			String line = "";
+//			
+//			if(nrOfProducts >= Integer.parseInt(inline)){
+//				for(int i = 1; i <= Integer.parseInt(inline); i++){
+//					line = storeText.readLine();
+//				}
+//				outstream.writeBytes("You have choosen:");
+//				outstream.writeBytes(line);
+//
+//				outstream.writeBytes("  "+line.indexOf(",") + "\n\r");
+//				outstream.writeBytes("  "+(line.substring(line.indexOf(",")+1, line.length()).indexOf(",")+line.indexOf(",")+1) + "\n\r");
+//				
+//				
+////				for(){
+////					
+////				}
+//				//String ProduktNr
+//				//produkt nr
+//				// produkt name
+//				//produkt total weight
+//
+//
+//			}else{
+//				return 0;
+//			}
+//
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return 0;
+//
+//
+//	}
+	
+	
+//	try {
+//		
+//
+//		storeText = new BufferedReader(new FileReader("Store.txt"));
+//
+//
+//		String line = storeText.readLine();
+//		
+//		int nrOfProducts = 0;
+//		while (line != null) {
+//			outstream.writeBytes(line +"\n\r");
+//			line = storeText.readLine();
+//			nrOfProducts++;
+//		}
+//		outstream.writeBytes("Please write a produkt ID - its a nr.");
+//		inline = instream.readLine().toUpperCase();
+//		if((inline.matches("[0-9]+"))){
+//			productVerification(inline, nrOfProducts);
+//		}else{
+//			System.out.println("Wrong input!");
+//		}
+//
+//	} catch (Exception e2) {
+//		// TODO Auto-generated catch block
+//		e2.printStackTrace();
+//	}
 	
 	
 }
