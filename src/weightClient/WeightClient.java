eeb638e49d4d5c5d8a324b85554987e269bcdb1d
Martin Vieth <martin_123_vieth@hotmail.com>
2015-04-11 12:44:49 +0200

    minor stuff in printMenu

--- src/weightClient/WeightClient.java
+++ src/weightClient/WeightClient.java
@@ -8,20 +8,38 @@ import java.util.Scanner;
 
 public class WeightClient {
 
-//	@SuppressWarnings("deprecation")
-	public void run() {
+	private static boolean active = true;
+
+	private static DataOutputStream outputStream = null;
+	private static InputStreamReader inputstream = null;
+	private static Socket socket = null;
+	private static Scanner scanner = new Scanner(System.in);
+	private static BufferedReader read;
+	private static PrintWriter write;
+
+	public static void main(String[] args) {
+		printMenu();
+		run();
 		
-		DataOutputStream outputStream = null;
-		InputStreamReader inputstream = null;
-		Socket socket = null;
-		Scanner scanner = new Scanner(System.in);
-		
+
+		while(active){
+			
+			
+
+		}
+
+	}
+
+	public static void run() {
+
+
+
 		//Data input
 		System.out.println("Input the server IP address: ");
 		String ip = (String) scanner.nextLine();
 		System.out.println("Input the server application port number: ");
 		String portDest = (String) scanner.nextLine();
-		
+
 		System.out.println("IP Address: " +ip);
 		System.out.println("Port#: " + portDest);
 
@@ -34,10 +52,10 @@ public class WeightClient {
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
-		
-		BufferedReader read = new BufferedReader(inputstream);
-		PrintWriter write = new PrintWriter(outputStream,true);
-		
+
+		read = new BufferedReader(inputstream);
+		write = new PrintWriter(outputStream,true);
+
 		//Check for om ports og sockets ikke er null, hvis de ikke er 
 		if(socket != null && outputStream != null && inputstream != null){
 			System.out.println("Connection Established");
@@ -51,7 +69,7 @@ public class WeightClient {
 					//this gets the reply from the server if any
 					String replyMess = read.readLine();
 					if(replyMess != null){
-					System.out.println(replyMess);
+						System.out.println(replyMess);
 					}				
 				} catch (UnknownHostException e) {
 					System.err.println("Error001: "+e.getMessage());
@@ -62,11 +80,17 @@ public class WeightClient {
 			scanner.close();
 		} else System.out.println("Unable to establish connection");
 	}
-	public void printMenu() {
-		System.out.println("Press S and ENTER to read scale");
+	
+	
+	public static void printMenu() {
+		System.out.println("Write RM20 8 \"<message>\" \"<message>\" \"<message>\"  to send a mesage to the scale and wait for a reply");
+		System.out.println("Write P111 \"<message>\" to send a message to the instruktion display(max 30 chars)");
+		System.out.println("Write D \"<message>\" and ENTER to display message on weight display");
+		System.out.println("Press B <weight> and ENTER to send a weight to scale");
+		System.out.println("Press S and ENTER to read scale (brutto - tara)");
 		System.out.println("Press T and ENTER to tare scale");
 		System.out.println("Press Z and ENTER to zero scale");
-		System.out.println("Write D \"<message>\" and ENTER to display message on weight display");
 		System.out.println("Write DW and ENTER to display weight on scale"); //doesnt work?
+		System.out.println("Write Q and ENTER to close everything.");
 	}	
 }
