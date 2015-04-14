99e51d2addb0bed4e205b8e77d47b79ee95f082b
Delvaux Paulin Niyonzima <s144849@student.dtu.dk>
2015-04-14 19:12:36 +0200

    Printmenu til Main

--- src/ftpClient/Main.java
+++ src/ftpClient/Main.java
@@ -2,6 +2,8 @@ package ftpClient;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 import weightClient.WeightClient;
 
@@ -37,8 +39,31 @@ public class Main {
 				System.out.println("Please write a command");
 				ftp.makeRequest();
 				ftp.sendRequest();
+				Scanner keyb = new Scanner(System.in);
+				System.out.println("Press 1 for FTP Client");
+				System.out.println("Press 2 for Weight Client");
 				
+				String input = " ";
+				try {
+					input = keyb.nextLine();
+				} catch (NoSuchElementException e1) {
+					System.out.println("Error: "+e1.getMessage());
+					//e1.printStackTrace();
+				}
 
+				if(!(input.isEmpty())) {
+					if(input.equals("1")) {
+						ftp.printMenu();
+					}
+					if(input.equals("2")) {
+						weightc.printMenu();
+					}else{
+						active = true;
+						continue;
+					}
+					
+				}
+				keyb.close();
 			}
 		}
 
